c10130b036d845fe35117867e92ccd45b02bf0a2
KIMUTAICHELANGA <briankimu97@gmail.com>
2024-03-30 18:58:57 +0300

    initial commit

---
+++ Configurations/config.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+@dataclass
+class Paths:
+    train_dir: str
+    test_dir: str
+    mlflow_tracking_uri: str
+    model_artifactory_dir: str
+    audio_dir: str
+
+@dataclass
+class Params:
+    epochs: int
+    learning_rate: float
+    test_data_split_percent: float
+    mfcc_length: int
+    sampling_rate: int
+    n_mfcc: int
+    batch_size: int
+
+@dataclass
+class Names:
+    experiment_name: str
+    audio_file: str
+    metric_name: str
+
+@dataclass
+class KWSconfig:
+     paths: Paths
+     params: Params
+     names: Names
---
+++ src/exception_handler.py
@@ -0,0 +1,11 @@
+class MLFlowError(Exception):
+    pass
+
+class ValueError(Exception):
+    pass
+
+class DirectoryError(Exception):
+    pass
+
+class NotFoundError(Exception):
+    pass
